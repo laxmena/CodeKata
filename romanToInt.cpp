4a74f8887e13e553a2783b9a987370b8b854e0d7
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2017-07-18 11:33:25 +0530

    Create romanToInt.cpp

---
+++ romanToInt.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int convert(string rnumeral){
+	stack<int> bin;
+	int sum = 0, temp;
+
+	map <char,int> r_to_int;
+	r_to_int['I'] = 1;
+	r_to_int['V'] = 5;
+	r_to_int['X'] = 10;
+	r_to_int['L'] = 50;
+	r_to_int['C'] = 100;
+	r_to_int['D'] = 500;
+	r_to_int['M'] = 1000;
+
+	bin.push(r_to_int[rnumeral[0]]);
+	for(int i=1;i<rnumeral.size();i++){
+			if(bin.top() < r_to_int[rnumeral[i]]){
+				temp = bin.top();
+				bin.pop();
+				bin.push(r_to_int[rnumeral[i]] - temp);
+			}
+			else
+				bin.push(r_to_int[rnumeral[i]]);
+	}
+
+	while(!bin.empty()){
+		sum += bin.top();
+		bin.pop();
+	}	
+
+	return sum;
+}
+
+int main(){
+	string roman;
+	cin>>roman;
+	cout<<convert(roman);
+	return 0;
+}
