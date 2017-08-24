9cb46fc73c6e51149a8e896872f4578716928e5f
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2017-08-24 23:59:17 +0530

    update compareTime.cpp

--- compareTime.cpp
+++ compareTime.cpp
@@ -9,9 +9,15 @@ int main(){
 	cout<<"Enter railway time in Format:\n h:m:s (eg:15:23:59)\n";
 	cout<<"Enter Time 1: ";
 	cin>>hrs>>c>>min>>c>>sec;
+	assert(hrs<25 && hrs>=0);
+	assert(min<61 && min>=0);
+	assert(sec<61 && sec>=0);
 	time1 = hrs*60*60 + min*60 + sec;
 	cout<<"Enter Time 2: ";
 	cin>>hrs>>c>>min>>c>>sec;
+	assert(hrs<25 && hrs>=0);
+	assert(min<61 && min>=0);
+	assert(sec<61 && sec>=0);
 	time2 = hrs*60*60 + min*60 + sec;
 	if(time1 == time2)
 		cout<<"Both the Times are Equal\n";
