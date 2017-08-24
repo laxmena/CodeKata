fa19942c23069c265c43c92dd330c571556e23b7
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2017-08-24 23:39:28 +0530

    Update deleteDigits.cpp

--- deleteDigits.cpp
+++ deleteDigits.cpp
@@ -13,6 +13,9 @@ int findMinimum(int num, int digits){
 	sort(splitArr.begin(), splitArr.end());
 
 	int resultLen = splitArr.size()-digits;
+
+	if(resultLen < 1) return 0;
+
 	pos = 0; num = 0;
 	while(resultLen){
 		num = num*10 + splitArr[pos];
@@ -26,8 +29,10 @@ int main(){
 	int num,digits;
 	cout<<"Enter input Number: ";
 	cin>>num;
+	assert(num>-1);
 	cout<<"Enter number of digits to remove: ";
 	cin>>digits;
+	assert(digits>-1);
 	cout<<findMinimum(num,digits)<<endl;
 	return 0;
 }
